b726c4e4ffe8f6e582773382633dc3bda5a11c07
Vaishnav Potlapalli <51985757+va1shn9v@users.noreply.github.com>
2021-07-14 18:44:13 +0530

    objects logs

--- ssd/data/datasets/evaluation/csv_data/__init__.py
+++ ssd/data/datasets/evaluation/csv_data/__init__.py
@@ -351,7 +351,7 @@ def calc_detection_voc_prec_rec(
 
         tp = np.cumsum(match_l == 1)
         fp = np.cumsum(match_l == 0)
-
+        print("Total objects in : {}".format(tp+fp))
         # If an element of fp + tp is 0,
         # the corresponding element of prec[l] is nan.
         prec[l] = tp / (fp + tp)
